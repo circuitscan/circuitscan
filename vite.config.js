b2e66de144a849b218695ce6059597badf7e3487
numtel <ben@latenightsketches.com>
2024-07-30 14:07:20 +0200

    Vite server proxy no longer used

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    proxy: {
-      // Needed to defeat CORS
-      '/api': {
-        target: 'http://localhost:9000', // Docker lambda server
-        changeOrigin: true, // needed for virtual hosted sites
-        rewrite: (path) => path.replace(/^\/api/, '/2015-03-31/functions/function/invocations'),
-      },
-    },
-  },
-
   plugins: [
     {
       name: 'treat-js-files-as-jsx',
