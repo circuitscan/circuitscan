c454d3f07443b0c8fdb41a13ac62a33df8e19026
numtel <ben@latenightsketches.com>
2024-08-01 15:04:07 +0200

    Intro on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import { useParams, Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { CodeBracketSquareIcon, PaperAirplaneIcon } from '@heroicons/react/24/solid';
+import {
+  CodeBracketSquareIcon,
+  BookOpenIcon,
+  MapIcon,
+  SpeakerWaveIcon,
+  UserGroupIcon,
+} from '@heroicons/react/24/solid';
 
 import Card from '../components/Card.js';
 import Newest from '../components/Newest.js';
@@ -11,6 +17,45 @@ export function Home() {
     <Helmet>
       <title>Circuitscan - Home</title>
     </Helmet>
+    <Card>
+      <p className="py-3">
+        Circuitscan provides the same safety guarantees to circuit verifier contracts as normal smart contracts receive by verifying their source code on a block explorer: guaranteed access to the application even if their frontend becomes unavailable as well as the ability to review the source code for potential issues.
+        <br /><br />
+        Supports all EVM chains. Use the CLI to verify or deploy your circuit verifiers.
+      </p>
+      <p className="pt-6">
+        <a href="https://circuitscan.readthedocs.io" target="_blank" rel="noopener">
+          <button className={clsButton}>
+            <BookOpenIcon className="h-9 w-9 text-lightaccent dark:text-darkaccent" />
+            Docs
+          </button>
+        </a>
+        <a href="https://medium.com/@circuitscan" target="_blank" rel="noopener">
+          <button className={clsButton}>
+            <SpeakerWaveIcon className="h-9 w-9 text-lightaccent dark:text-darkaccent" />
+            Blog
+          </button>
+        </a>
+        <a href="https://github.com/circuitscan/cli" target="_blank" rel="noopener">
+          <button className={clsButton}>
+            <CodeBracketSquareIcon className="h-9 w-9 text-lightaccent dark:text-darkaccent" />
+            CLI
+          </button>
+        </a>
+        <a href="https://t.me/circuitscan" target="_blank" rel="noopener">
+          <button className={clsButton}>
+            <UserGroupIcon className="h-9 w-9 text-lightaccent dark:text-darkaccent" />
+            Telegram
+          </button>
+        </a>
+        <a href="https://docs.google.com/spreadsheets/d/1r22REpvo1jRHmiWcIwlbdT8QI7kh8DBKz21RHFhDz0E/edit?usp=sharing" target="_blank" rel="noopener">
+          <button className={clsButton}>
+            <MapIcon className="h-9 w-9 text-lightaccent dark:text-darkaccent" />
+            Roadmap
+          </button>
+        </a>
+      </p>
+    </Card>
     <Card>
       <h3 className={`
         text-xl pt-6 pb-3
